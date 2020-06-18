50f5c29c6cfe447ce1261ed7c583a0abae0b8199
Guilhem Bryant <Guilhem.Bryant@arm.com>
2020-06-18 13:15:33 +0100

    Comment MBEDTLS_SSL_EXPORT_KEYS back

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -2025,7 +2025,7 @@
  *
  * Comment this macro to disable support for key export
  */
-#define MBEDTLS_SSL_EXPORT_KEYS
+//#define MBEDTLS_SSL_EXPORT_KEYS
 
 /**
  * \def MBEDTLS_SSL_SERVER_NAME_INDICATION
