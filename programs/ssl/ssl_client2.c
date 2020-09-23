183d12b8bb014686842491555946bd233193953b
Hanno Becker <hanno.becker@arm.com>
2020-09-23 11:12:13 +0100

    Minor style improvement in ssl_client2.c
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- programs/ssl/ssl_client2.c
+++ programs/ssl/ssl_client2.c
@@ -622,8 +622,8 @@ struct options
     int force_srtp_profile;     /* SRTP protection profile to use or all    */
     const char *mki;            /* The dtls mki value to use                */
 #if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
-    const char *named_groups_string;    /* list of named groups             */
-    const char *key_share_named_groups_string;    /* list of named groups   */
+    const char *named_groups_string;           /* list of named groups      */
+    const char *key_share_named_groups_string; /* list of named groups      */
     int key_exchange_modes;     /* supported key exchange modes             */
 #if defined(MBEDTLS_ZERO_RTT)
     int early_data;             /* support for early data                   */
