730597e0491a47c461f712b7b0bb90f1d4aeda71
Hanno Becker <hanno.becker@arm.com>
2021-02-01 09:03:38 +0000

    Free SSL transforms on SSL context destruction/reset
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -4179,12 +4179,14 @@ static int ssl_handshake_init( mbedtls_ssl_context *ssl )
 #endif
 
 #if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
+        mbedtls_ssl_transform_free( ssl->transform_handshake   );
+        mbedtls_ssl_transform_free( ssl->transform_earlydata   );
+        mbedtls_ssl_transform_free( ssl->transform_application );
         mbedtls_free( ssl->transform_handshake   );
         mbedtls_free( ssl->transform_earlydata   );
         mbedtls_free( ssl->transform_application );
         ssl->transform_handshake   = NULL;
         ssl->transform_earlydata   = NULL;
-
         ssl->transform_application = NULL;
 #endif
 
@@ -4618,6 +4620,18 @@ int mbedtls_ssl_session_reset_int( mbedtls_ssl_context *ssl, int partial )
         ssl->transform = NULL;
     }
 #endif /* MBEDTLS_SSL_PROTO_TLS1_2_OR_EARLIER */
+
+#if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
+    mbedtls_ssl_transform_free( ssl->transform_handshake   );
+    mbedtls_ssl_transform_free( ssl->transform_earlydata   );
+    mbedtls_ssl_transform_free( ssl->transform_application );
+    mbedtls_free( ssl->transform_handshake   );
+    mbedtls_free( ssl->transform_earlydata   );
+    mbedtls_free( ssl->transform_application );
+    ssl->transform_handshake   = NULL;
+    ssl->transform_earlydata   = NULL;
+    ssl->transform_application = NULL;
+#endif
 
     if( ssl->session )
     {
@@ -7589,6 +7603,18 @@ void mbedtls_ssl_free( mbedtls_ssl_context *ssl )
     }
 #endif
 
+#if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
+    mbedtls_ssl_transform_free( ssl->transform_handshake   );
+    mbedtls_ssl_transform_free( ssl->transform_earlydata   );
+    mbedtls_ssl_transform_free( ssl->transform_application );
+    mbedtls_free( ssl->transform_handshake   );
+    mbedtls_free( ssl->transform_earlydata   );
+    mbedtls_free( ssl->transform_application );
+    ssl->transform_handshake   = NULL;
+    ssl->transform_earlydata   = NULL;
+    ssl->transform_application = NULL;
+#endif
+
     if( ssl->handshake )
     {
         mbedtls_ssl_handshake_free( ssl );
@@ -7606,6 +7632,9 @@ void mbedtls_ssl_free( mbedtls_ssl_context *ssl )
         mbedtls_free( ssl->transform_handshake   );
         mbedtls_free( ssl->transform_earlydata   );
         mbedtls_free( ssl->transform_application );
+        ssl->transform_handshake   = NULL;
+        ssl->transform_earlydata   = NULL;
+        ssl->transform_application = NULL;
 #endif
 
         mbedtls_ssl_session_free( ssl->session_negotiate );
