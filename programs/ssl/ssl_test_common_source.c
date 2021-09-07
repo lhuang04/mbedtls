1f04a69cb7c70ec1a194468ab8e044ed72409011
lhuang04 <lhuang04@fb.com>
2021-09-07 04:47:56 -0700

    Fix typo in nss_keylog_export
    Summary:
    `CLIENT RANDOM` -> `CLIENT_RANDOM`. This appears a typo as it is
    [CLIENT_RANDOM](https://github.com/ARMmbed/mbedtls/blob/development/programs/ssl/ssl_test_common_source.c#L72) in upstream and in the
    [document](https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/Key_Log_Format).
    
    Test Plan:
    `tests/ssl-opt.sh`
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- programs/ssl/ssl_test_common_source.c
+++ programs/ssl/ssl_test_common_source.c
@@ -68,7 +68,7 @@ void nss_keylog_export( void *p_expkey,
     switch( secret_type )
     {
         case MBEDTLS_SSL_KEY_EXPORT_TLS12_MASTER_SECRET:
-            strcpy(label, "CLIENT RANDOM ");
+            strcpy(label, "CLIENT_RANDOM ");
             break;
         case MBEDTLS_SSL_KEY_EXPORT_TLS13_CLIENT_EARLY_SECRET:
             strcpy(label, "CLIENT_EARLY_TRAFFIC_SECRET ");
