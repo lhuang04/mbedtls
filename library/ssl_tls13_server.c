78a04ce344da7682d4afe180b1d503e4d74173f6
lhuang04 <lhuang04@fb.com>
2023-05-26 07:29:29 -0700

    replace mbedtls_ssl_safer_memcmp
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- library/ssl_tls13_server.c
+++ library/ssl_tls13_server.c
@@ -591,7 +591,7 @@ int mbedtls_ssl_parse_client_psk_identity_ext(
             /* Identity is not a big secret since clients send it in the clear,
              * but treat it carefully anyway, just in case */
             if( item_length != ssl->conf->psk_identity_len ||
-                mbedtls_ssl_safer_memcmp( ssl->conf->psk_identity, buf, item_length ) != 0 )
+                mbedtls_ct_memcmp( ssl->conf->psk_identity, buf, item_length ) != 0 )
             {
                 ret = MBEDTLS_ERR_SSL_UNKNOWN_IDENTITY;
             }
@@ -844,7 +844,7 @@ psk_parsing_successful:
         MBEDTLS_SSL_DEBUG_BUF( 3, "psk binder ( received ): ",
                                buf, item_length );
 
-        if( mbedtls_ssl_safer_memcmp( server_computed_binder, buf,
+        if( mbedtls_ct_memcmp( server_computed_binder, buf,
                                       item_length ) != 0 )
         {
             MBEDTLS_SSL_DEBUG_MSG( 1,
